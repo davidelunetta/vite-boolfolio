7aab406cc9691ffa78b6ddfd638f5ff03f5ee1ef
davidelunetta <davidelunetta.27@gmail.com>
2024-01-23 11:34:12 +0100

    modified api url

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiBaseUrl: "http://127.0.0.1:8000/api",
+    apiBaseUrl: "http://127.0.0.1:8000/admin/api/projects",
     image_path: "http://127.0.0.1:8000/storage/",
     projects:[]
 
