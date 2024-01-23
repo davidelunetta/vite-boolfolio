6371ee98d1eb2bf9ad43e8b6ce75e4a7a2787519
davidelunetta <davidelunetta.27@gmail.com>
2024-01-23 11:35:22 +0100

    added template + function

--- src/pages/AppPosts.vue
+++ src/pages/AppPosts.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>Project List</h1>
+        <ul>
+            <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+        </ul>
     </div>
 </template>
 
@@ -12,11 +15,16 @@ export default {
     data(){
         return{
             store,
+            projects: [], 
         }
+    },
+    mounted() {
+        this.getAllPosts(); 
     },
     methods: {
         getAllPosts(){
             axios.get(`${this.store.apiBaseUrl}projects`).then((res)=>{
+                this.projects = res.data.result;
                 console.log(res.data);
             }).catch((err)=>{
 
